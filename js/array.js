d4de9bd52906117fc52b1ee1ffdfb3d91a109eb6
George Salu <george.salu10@gmail.com>
2018-11-05 15:04:41 -0200

    inserindo elementos

--- js/array.js
+++ js/array.js
@@ -26,3 +26,20 @@ for (var i = 3; i < 20; i++) {
 }
 
 console.log('Fibonacci ' + fibonacci)
+
+// inserindo elementos
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numbers[10] = 10
+numbers.push(12)
+numbers.push(13)
+numbers.push(14)
+numbers.push(15)
+numbers.push(16)
+
+numbers.unshift(-1)
+numbers.unshift(-2)
+numbers.unshift(-3)
+numbers.unshift(-4)
+
+console.log(numbers)
