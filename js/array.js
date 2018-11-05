2d7c766298bf593e8da80b45018bf8f41f79230f
George Salu <george.salu10@gmail.com>
2018-11-05 14:59:05 -0200

    acessando elementos atraves da interação

--- js/array.js
+++ js/array.js
@@ -1,4 +1,4 @@
-var avgTemp = []
+/*var avgTemp = []
 
 avgTemp[0] = 31.9
 avgTemp[1] = 35.9
@@ -7,8 +7,22 @@ avgTemp[3] = 38
 avgTemp[4] = 25.5
 
 console.log(avgTemp[0])
+*/
 
+var daysOfWeek = ['Sunday', 'Monday', 'Yuesday', 'Wednesday', 'Trusday', 'Friday', 'Saturday'];
 
-var daysOfWeek = new Array('Sunday', 'Monday', 'Yuesday', 'Wednesday', 'Trusday', 'Friday', 'Saturday');
+for (var i = 0; i < daysOfWeek.length; i++) {
+    console.log(daysOfWeek[i])
+}
 
-console.log(daysOfWeek[3])
+// fibonacci
+var fibonacci = []
+fibonacci[0] = 0
+fibonacci[1] = 1
+fibonacci[2] = 1
+
+for (var i = 3; i < 20; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+}
+
+console.log('Fibonacci ' + fibonacci)
