beb97fc82fef1685ef872e0e8d2342d9957d13ab
George Salu <george.salu10@gmail.com>
2018-11-05 21:20:38 -0200

    introdução a pilha

---
+++ js/pilhas.js
@@ -0,0 +1,31 @@
+function Stack() {
+    var items = []
+
+    this.push = function (element) {
+        //adiciona um novo item a pilha
+    }
+
+    this.pop = function () {
+        //remove o item do topo da pilha
+    }
+
+    this.peek = function () {
+        //devolve o elemento que esta no topo da pilha
+    }
+
+    this.isEmpty = function () {
+        //informa se a pilha esta vazia ou nao
+    }
+
+    this.clear = function () {
+        //limpa a pilha
+    }
+
+    this.size = function () {
+        //informa o tamanho da pilha
+    }
+
+    this.print = function () {
+        //imprime a pilha no console
+    }
+}
