131f4d96ba6fb3854894a5cffd7d0f63d94df9be
George Salu <george.salu10@gmail.com>
2018-11-06 18:47:18 -0200

    introdução a fila

---
+++ js/fila/fila.js
@@ -0,0 +1,27 @@
+function Queue() {
+    var items = [];
+
+    this.enqueue = function () {
+        //adiona novo item
+    }
+
+    this.dequeue = function () {
+        //remove um item
+    }
+
+    this.front = function () {
+        //retorna o primeiro elemento da fila
+    }
+
+    this.isEmpty = function () {
+        //verifica se a fila esta vazia ou nao
+    }
+
+    this.size = function () {
+        //retorna o tamanho da fila 
+    }
+
+    this.print = function () {
+        //imprimir a fila no console
+    }
+}
