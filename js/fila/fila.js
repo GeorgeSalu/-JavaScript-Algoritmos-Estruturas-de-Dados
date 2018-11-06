2df5a30ae5fde0ce62405f6a927579cb2ec75354
George Salu <george.salu10@gmail.com>
2018-11-06 18:55:08 -0200

    implementação inicial

--- js/fila/fila.js
+++ js/fila/fila.js
@@ -1,27 +1,33 @@
 function Queue() {
     var items = [];
 
-    this.enqueue = function () {
+    this.enqueue = function (element) {
         //adiona novo item
+        items.push(element);
     }
 
     this.dequeue = function () {
         //remove um item
+        return items.shift();
     }
 
     this.front = function () {
         //retorna o primeiro elemento da fila
+        return items[0]
     }
 
     this.isEmpty = function () {
         //verifica se a fila esta vazia ou nao
+        return items.length === 0;
     }
 
     this.size = function () {
-        //retorna o tamanho da fila 
+        //retorna o tamanho da fila
+        return items.length;
     }
 
     this.print = function () {
         //imprimir a fila no console
+        console.log(items.toString());
     }
 }
