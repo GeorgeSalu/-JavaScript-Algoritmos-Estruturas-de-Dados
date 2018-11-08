4b9b77ab3ba2089fb9fc2ac02715e5869cb45135
George Salu <george.salu10@gmail.com>
2018-11-08 20:54:53 -0200

    fila circular pt2

--- js/fila/filaCircular.js
+++ js/fila/filaCircular.js
@@ -1,3 +1,37 @@
+function Queue() {
+    var items = [];
+
+    this.enqueue = function (element) {
+        //adiona novo item
+        items.push(element);
+    }
+
+    this.dequeue = function () {
+        //remove um item
+        return items.shift();
+    }
+
+    this.front = function () {
+        //retorna o primeiro elemento da fila
+        return items[0]
+    }
+
+    this.isEmpty = function () {
+        //verifica se a fila esta vazia ou nao
+        return items.length === 0;
+    }
+
+    this.size = function () {
+        //retorna o tamanho da fila
+        return items.length;
+    }
+
+    this.print = function () {
+        //imprimir a fila no console
+        console.log(items.toString());
+    }
+}
+
 function hotPotato(nameList, num) {
     var queue = new Queue()
 
@@ -17,3 +51,7 @@ function hotPotato(nameList, num) {
 
     return queue.dequeue()
 }
+
+var names = ['joao', 'jose', 'maria', 'ana', 'lucas']
+var winner = hotPotato(names, 7)
+console.log('this winner is : ' + winner)
