66348981be2549ff8d84ab4628dd592d0ab76db9
George Salu <george.salu10@gmail.com>
2019-01-18 19:08:44 -0200

    metodo difference

--- js/conjuntos/conjuntos.js
+++ js/conjuntos/conjuntos.js
@@ -72,6 +72,18 @@ function Set() {
         }
         return intersectionSet
     }
+
+    this.difference = function (otherSet) {
+        var differenceSet = new Set(),
+            values = this.values()
+
+        for (var i = 0; i < values.length; i++) {
+            if (!otherSet.has(values[i])) {
+                differenceSet.add(values[i])
+            }
+        }
+        return differenceSet
+    }
 }
 
 var setA = new Set()
