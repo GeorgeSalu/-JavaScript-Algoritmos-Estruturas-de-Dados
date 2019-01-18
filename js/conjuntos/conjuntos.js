b0e09f6894506e542bc1a7f6da6d0d5e9f0bcf9a
George Salu <george.salu10@gmail.com>
2019-01-18 19:25:22 -0200

    metodo subset pt2

--- js/conjuntos/conjuntos.js
+++ js/conjuntos/conjuntos.js
@@ -102,17 +102,18 @@ function Set() {
 }
 
 var setA = new Set()
-
 setA.add(1)
 setA.add(2)
-setA.add(3)
-setA.add(4)
-setA.add(4)
 
 var setB = new Set()
+setB.add(1)
 setB.add(2)
 setB.add(3)
-setB.add(4)
 
-var differenceAB = setA.difference(setB)
-console.log(differenceAB.values())
+var setC = new Set()
+setC.add(2)
+setC.add(3)
+setC.add(4)
+
+console.log(setA.subset(setB))
+console.log(setA.subset(setC))
