9e0f8ba69ce19037fafdeeef5dd70ccfb5d7561a
George Salu <george.salu10@gmail.com>
2019-01-19 19:34:29 -0200

    introdução aos dicionarios

---
+++ js/dicionario/dicionario-e-hash.js
@@ -0,0 +1,35 @@
+function Dictionary() {
+    var items = {}
+
+    this.set = function (key, value) {
+        //adiciona um novo item ao dicionario
+    }
+
+    this.delete = function (key) {
+        //remove o valor do dicionario usando a chave
+    }
+
+    this.has = function (key) {
+        //verifica se a chave existe e retorna um boolean
+    }
+
+    this.get = function (key) {
+        //devolve um valor especifico a partir da chave
+    }
+
+    this.clear = function () {
+        //remove todos os items do dicionario
+    }
+
+    this.size = function () {
+        //retorna a quantidade de elementos contidos no dicionario
+    }
+
+    this.keys = function () {
+        //devolve um array com todas as chaves do dicionario
+    }
+
+    this.values = function () {
+        //devolve um array com todos os valores do dicionario
+    }
+}
