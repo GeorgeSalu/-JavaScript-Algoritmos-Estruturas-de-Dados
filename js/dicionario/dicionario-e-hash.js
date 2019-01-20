7b74f3860c529a7fdcd521120edb283d6159becb
George Salu <george.salu10@gmail.com>
2019-01-20 09:49:48 -0200

    tratando colisao pt2

--- js/dicionario/dicionario-e-hash.js
+++ js/dicionario/dicionario-e-hash.js
@@ -1,3 +1,135 @@
+function LinkedList() {
+    var Node = function (element) {
+        this.element = element;
+        this.next = null
+    }
+
+    var length = 0;
+    var head = null;
+
+    this.append = function (element) {
+        //adiciona um elemento no final da lista
+        var node = new Node(element),
+            current
+
+        if (head === null) {
+            head = node
+        } else {
+            current = head
+
+            while (current.next) {
+                current = current.next
+            }
+
+            current.next = node
+        }
+        length++
+    }
+
+    this.insert = function (position, element) {
+        //adiciona um elemento em uma posicao especifica
+        if (position >= 0 && position <= length) {
+            var node = new Node(element),
+                current = head,
+                previos,
+                index = 0;
+
+            if (position === 0) {
+                node.next = current
+                head = node
+            } else {
+                while (index++ < position) {
+                    previos = current
+                    current = current.next
+                }
+                node.next = current
+                previos.next = node
+            }
+            length++
+            return true
+        } else {
+            return false
+        }
+    }
+
+    this.removeAt = function (position) {
+        //remove o elemento de uma posicao especifica
+        if (position > -1 && position < length) {
+            var current = head,
+                previos,
+                index = 0
+
+            if (position === 0) {
+                head = current.next
+            } else {
+                while (index++ < position) {
+                    previos = current
+                    current = current.next
+                }
+                previos.next = current.next
+            }
+            length--
+            return current.element;
+        } else {
+            return null;
+        }
+    }
+
+    this.remove = function (element) {
+        //remove o elemento element
+        var index = this.indexOf(element)
+        return this.removeAt(index)
+    }
+
+    this.indexOf = function (element) {
+        //retorna a posicao do elemento
+        var current = head,
+            index = 0;
+
+        while (current) {
+            if (element === current.element) {
+                return index;
+            }
+            index++
+            current = current.next
+        }
+        return - 1
+    }
+
+    this.isEmpty = function () {
+        //retorna se esta vazia ou nao
+        return length === 0
+    }
+
+    this.size = function () {
+        //retorna o tamanho da instancia
+        return length;
+    }
+
+    this.getHead = function () {
+        return head;
+    }
+
+    this.toString = function () {
+        //converte em string
+        var current = head,
+            string = '';
+
+        while (current) {
+            string += current.element + ' '
+            current = current.next
+        }
+
+        return string
+    }
+
+    this.print = function () {
+        //imprime no console
+        console.log(this.toString());
+    }
+}
+
+
 function Dictionary() {
     var items = {}
 
@@ -56,11 +188,29 @@ function Dictionary() {
 function HashTable() {
     var table = []
 
-    this.put = function (key, value) {
+
+    var valuePair = function (key, value) {
+        this.key = key
+        this.value = value
+        this.toString = function () {
+            return '[' + this.key + ' - ' + this.value + ']'
+        }
+    }
+
+    /*this.put = function (key, value) {
         //insere elemento
         var position = loseloseHashCode(key)
         console.log(position + ' ' + key)
         table[position] = value
+    }*/
+    this.put = function (key, value) {
+        //insere elemento
+        var position = loseloseHashCode(key)
+
+        if (table[position] === undefined) {
+            table[position] = new LinkedList()
+        }
+        table[position].append(new valuePair(key, value));
     }
 
     this.remove = function (key) {
