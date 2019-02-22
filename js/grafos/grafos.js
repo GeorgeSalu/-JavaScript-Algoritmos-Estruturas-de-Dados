e3745b20267078f427b75c216d4769364cb01af5
George Salu <george.salu10@gmail.com>
2019-02-22 10:48:53 -0300

    implementando os metodos

--- js/grafos/grafos.js
+++ js/grafos/grafos.js
@@ -1,14 +1,79 @@
+function Dictionary() {
+    var items = {}
+
+    this.set = function (key, value) {
+        //adiciona um novo item ao dicionario
+        items[key] = value
+    }
+
+    this.delete = function (key) {
+        //remove o valor do dicionario usando a chave
+        if (this.has(key)) {
+            delete items[key]
+            return true
+        }
+        return false
+    }
+
+    this.has = function (key) {
+        //verifica se a chave existe e retorna um boolean
+        return items.hasOwnProperty(key);
+    }
+
+    this.get = function (key) {
+        //devolve um valor especifico a partir da chave
+        return this.has(key) ? items[key] : undefined
+    }
+
+    this.clear = function () {
+        //remove todos os items do dicionario
+        items = {}
+    }
+
+    this.size = function () {
+        //retorna a quantidade de elementos contidos no dicionario
+        return Object.keys(items).length
+    }
+
+    this.keys = function () {
+        //devolve um array com todas as chaves do dicionario
+        return Object.keys(items)
+    }
+
+    this.values = function () {
+        //devolve um array com todos os valores do dicionario
+        var values = [],
+            keys = Object.keys(items)
+
+        for (var i = 0; i < keys.length; i++) {
+            values.push(items[keys[i]])
+        }
+        return values
+    }
+}
+
 function Graph() {
 
     this.addVertex = function (v) {
-
+        var vertices = []
+        var adjList = new Dictionary()
     }
 
     this.addEdge = function (v, w) {
-
+        adjList.get(v).push(w)
+        adjList.get(w).push(v)
     }
 
     this.toString = function () {
-
+        var s = ''
+        for (var i = 0; i < vertices.length; i++) {
+            s += vertices[i] + ' -> '
+            var neighbors = adjList.get(vertices[i])
+            for (var j = 0; j < neighbors.length; j++) {
+                s += neighbors[j] + ' '
+            }
+            s += '\n'
+        }
+        return s
     }
 }
