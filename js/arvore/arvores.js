6fde7fc774b8925c197f4aff133da7d29a7f43d4
George Salu <george.salu10@gmail.com>
2019-02-22 18:16:25 -0300

    implementando os metodos

---
+++ js/arvore/arvores.js
@@ -0,0 +1,40 @@
+function BinarySearchTree() {
+    var Node = function (key) {
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+    var root = null
+
+    this.insert = function () {
+        //insere uma chave
+    }
+
+    this.search = function () {
+        //busca uma chave 
+    }
+
+    this.remove = function () {
+        //remove a chave
+    }
+
+    this.min = function () {
+        //retorna a menor chave
+    }
+
+    this.max = function () {
+        //retorna a maior chave
+    }
+
+    this.inOrderTraverse = function () {
+        //visita todos os nos da arvore usando um percurso em ordem
+    }
+
+    this.preOrderTraverse = function () {
+        //visita todos os nos da arvore usando um percurso pre ordem
+    }
+
+    this.postOrderTraverse = function () {
+        //visita todos os nos da arvore usando um percurso pos ordem
+    }
+}
