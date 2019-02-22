5d3dab92b9eed60daf426d3ed600228e7bbb1a2f
George Salu <george.salu10@gmail.com>
2019-02-22 11:45:07 -0300

    selectionSort

--- js/ordenação/ordenacao.js
+++ js/ordenação/ordenacao.js
@@ -19,6 +19,21 @@ function ArrayList() {
         }
     }
 
+    this.selectionSort = function () {
+        var indexMin
+        for (var i = 0; i < array.length - 1; i++) {
+            indexMin = i
+            for (var j = i; j < array.length; j++) {
+                if (array[indexMin] > array[j]) {
+                    indexMin = j
+                }
+            }
+            if (i !== indexMin) {
+                swap(array, i, indexMin)
+            }
+        }
+    }
+
     var swap = function (array, index1, index2) {
         var aux = array[index1]
         array[index1] = array[index2]
